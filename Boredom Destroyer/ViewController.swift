b75c8a6806b01769a84cb2cd3964978c22e7138b
Chopsticks8 <54952169+Chopsticks8@users.noreply.github.com>
2020-09-03 23:14:33 -0400

    Initial Commit

---
+++ Boredom Destroyer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Boredom Destroyer
+//
+//  Created by Evan Kwak on 2020-09-03.
+//  Copyright © 2020 Evan Kwak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
